d538033ff751b21258ec3e34a2c01cf5f48f574d
Andreeva Anastasia <32935561+aam-fd@users.noreply.github.com>
2018-11-25 01:36:45 +0300

    app markup, styles and logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,121 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
+import { SelectionModel } from '@angular/cdk/collections';
+
+import { Observable, Subscription } from 'rxjs';
+
+import { ProductsService } from './common/services/products.service';
+import { IProduct } from './common/types/products';
+import { getProductData } from './common/utils/utils';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'product-table';
+export class AppComponent implements OnInit, OnDestroy {
+
+  // subscription
+  public products$: Observable<any[]>;
+  private _subscriptionProducts: Subscription;
+
+  // data
+  public products: IProduct[] = [];
+
+  // ui
+  public selectedProducts: IProduct[] = [];
+
+  public dataProducts: MatTableDataSource<IProduct>;
+  public selection: SelectionModel<IProduct> = new SelectionModel<IProduct>(true, []);
+
+  public selectedProductIndex: number;
+
+  public displayedColumns: string[] = ['select', 'group', 'name', 'price'];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  public constructor(
+    private _productsService: ProductsService
+  ) {
+    this.products$ = this._productsService.getProducts();
+  }
+
+  public ngOnInit(): void {
+    this._subscriptionProducts = this.products$.subscribe((products) => {
+      products.forEach(productUnit => {
+        productUnit.skus.forEach(product => {
+          const productData: IProduct = getProductData(product, productUnit.group.name);
+          this.products.push(productData);
+        });
+      });
+
+      this.initTableData();
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this._subscriptionProducts.unsubscribe();
+  }
+
+  public initTableData(): void {
+    const notSelectedProducts: IProduct[] = this.products.filter((object: IProduct) => {
+      return object.selected === false;
+    });
+    this.dataProducts = new MatTableDataSource<IProduct>(notSelectedProducts);
+    this.dataProducts.paginator = this.paginator;
+    this.selection = new SelectionModel<IProduct>(true, []);    
+  }
+
+  public addToBasket(): void {
+    this.selectedProducts = this.selectedProducts.concat(this.selection.selected);
+    this.selection.selected.forEach((object: IProduct) => {
+      const selectedProductIndex: number = this.products.indexOf(object);
+      if (selectedProductIndex !== -1) {
+        object.selected = true;
+      }
+    });
+    this.initTableData();
+  }
+
+  public clearProductBasket(): void {
+    this.products.forEach((object: IProduct) => {
+      if (object.selected) {
+        object.selected = false;
+      }
+    });
+    this.selectedProducts = [];
+    this.initTableData();
+  }
+
+  // Whether the number of selected elements matches the total number of rows.
+  public isAllSelected(): boolean {
+    const numSelected: number = this.selection.selected.length;
+    const numRows: number = this.dataProducts.paginator.pageSize;
+
+    if (this.dataProducts.data.length < numRows) {
+      return this.dataProducts.data.length === numSelected;
+    }
+    return numSelected === numRows;
+  }
+  
+  // Selects all rows if they are not all selected; otherwise clear selection.
+  public masterToggle(): void {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataProducts.data.forEach((row, index) => {
+        this.selectProduct(row, index);
+      });
+  }
+
+  public selectProduct(row, index): void {
+    const pageIndex: number = this.dataProducts.paginator.pageIndex;
+    const pageSize: number = this.dataProducts.paginator.pageSize;
+    const startPoint: number = pageIndex * pageSize;
+    const endPoint: number = (pageIndex + 1) * pageSize;
+
+    if (index >= startPoint && index < endPoint) {
+      this.selection.select(row)
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { 
+  MatBadgeModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatIconModule,
+  MatPaginatorModule,
+  MatSelectModule,
+  MatTableModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ProductsService } from './common/services/products.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatSelectModule,
+    MatTableModule,
   ],
-  providers: [],
+  
+  providers: [
+    ProductsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
